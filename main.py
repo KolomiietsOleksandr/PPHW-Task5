844d78ff168b5b4ce1a984092ddccbac5ed22320
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-11-05 17:27:12 +0200

    Add function with processes

--- main.py
+++ main.py
@@ -16,3 +16,33 @@ def write_to_file(file_name, data):
         print(f"Result saved to '{file_name}'.")
     except Exception as e:
         print(f"Error: {e}")
+
+def process(user_input):
+    if user_input == "1":
+        data = input("Enter the data for encryption/decryption: ")
+    elif user_input == "2":
+        file_name = input("Enter file name: ")
+        data = read_from_file(file_name)
+        if data is None:
+            return
+    else:
+        print("False choice")
+        return
+
+    key = input("Enter key: ")
+
+    operation = input("Select the operation (encrypt - 'e' or decrypt - 'd'): ")
+
+    if operation == 'e':
+        result = encrypt(data, key)
+    elif operation == 'd':
+        result = decrypt(data, key)
+    else:
+        print("False operation")
+        return
+
+    output_file_name = input("Enter a file name to save the result (or press Enter to not save to a file): ")
+    if output_file_name:
+        write_to_file(output_file_name, result)
+    else:
+        print("Result:", result)
